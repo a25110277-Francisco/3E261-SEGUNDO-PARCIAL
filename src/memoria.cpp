0cf403f74bfa8dd93d44f31440fe36ce3a339684
a25110277-Francisco <a25110277@ceti.mx>
2026-04-29 03:26:21 +0000

    PRACTICA 6

---
+++ src/memoria.cpp
@@ -0,0 +1,33 @@
+//memoria estatica
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "long: " << sizeof(long) << endl;
+    cout << "int: " << sizeof(int) << endl;
+    cout << "char: " << sizeof(char) << endl;
+    cout << "bool: " << sizeof(bool) << endl;
+    cout << "float " << sizeof(float) << endl;
+    cout << "double: " << sizeof(double) << endl;
+
+    int a = 5;
+    cout << &a << endl; //& direccion de memoria
+    cout << a << endl; //valor de la variable
+    cout << *(&a) << endl; //accede al contenido de la memoria (* operador indirección)
+
+    cout << "long: " << sizeof(long *) << endl;
+    cout << "int: " << sizeof(int *) << endl;
+    cout << "char: " << sizeof(char *) << endl;
+    cout << "bool: " << sizeof(bool *) << endl;
+    cout << "float " << sizeof(float *) << endl;
+    cout << "double: " << sizeof(double *) << endl;
+
+    int * direccion; //direccion de un entero 8 bites es un tipo de dato
+    cout << direccion << endl; //que hay dentro de la memoria
+    cout << &direccion << endl; // dirección de la memoria
+    cout << *direccion << endl;
+
+    return 0;
+}
